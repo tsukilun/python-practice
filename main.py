1c2b5ee529ca8892efbcab548a9e2607f8a96f26
tsukilun <saltanattlegenn@gmail.com>
2026-03-26 09:38:22 +0500

    sos

---
+++ main.py
@@ -0,0 +1,40 @@
+print("Enter customer name:")
+customer_name = input()
+
+print("Enter product name:")
+product_name = input()
+
+print("Enter price per unit (KZT):")
+price_per_unit = float(input())
+
+print("Enter quantity:")
+quantity = int(input())
+
+subtotal = price_per_unit * quantity
+
+if subtotal > 5000:
+    discount = subtotal * 0.1
+else:
+    discount = 0
+
+total = subtotal - discount
+
+print("="*30)
+print("SHOP RECEIPT".center(30))
+print("="*30)
+
+print(f"Customer: {customer_name}")
+print(f"Product: {product_name}")   
+print(f"Price per unit: {price_per_unit} KZT")
+print(f"Quantity: {quantity}")
+
+print("-"*30)
+
+print(f"Subtotal: {subtotal} KZT")
+print(f"Discount: {discount} KZT")
+print(f"Total: {total} KZT")
+
+print("="*30)
+
+print("Discount applied:", subtotal > 5000)
+print("Paid more than 3000:", total > 3000)
